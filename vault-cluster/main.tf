c768f877029678498b2d2405c4ff8bde6b550fa8
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-17 00:12:31 +0800

    write tf files

---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = "SecureInfraOps-Project"
+  cloud_provider = "aws"
+  region         = var.aws_region
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = "SecureInfraOps-Project-vault-cluster"
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
+  tier       = "starter_small"
+  public_endpoint = true
+}
