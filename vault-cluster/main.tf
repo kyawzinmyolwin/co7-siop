8800f6e28fe6f72367eb5f9b515493e1a2eeab0b
kyawzinmyolwin <159759310+kyawzinmyolwin@users.noreply.github.com>
2024-09-17 10:37:56 +0800

    direct edit from repo 3rd time

--- vault-cluster/main.tf
+++ vault-cluster/main.tf
@@ -1,7 +1,7 @@
 resource "hcp_hvn" "vault_hvn" {
   hvn_id         = "SecureInfraOps-Project"
   cloud_provider = "aws"
-  region         = var.aws_region
+  region         = "ap-southeast-1"
   cidr_block     = "172.25.16.0/20"
 }
 
