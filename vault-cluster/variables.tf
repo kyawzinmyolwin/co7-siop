c7e47b3f0a35edf7e4ead37e0be5d060b1fe5782
kyazinmyolwin <kyawzin.workspace@gmail.com>
2024-09-17 14:54:54 +0800

    Test from vscode local 6th time from mbp

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  description = "Test from vscode local 5th time"
+  description = "Test from vscode local 6th time from mbp"
   default     = "ap-southeast-1"
 }
