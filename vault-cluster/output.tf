1a5af5cb4a530134014da0fdb445af3ef76c196d
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2025-03-01 15:49:02 +0000

    update vault cluster

--- vault-cluster/output.tf
+++ vault-cluster/output.tf
@@ -1,29 +1,14 @@
-output "hvn_id" {
-  description = "HCP HVN ID"
-  value = hcp_hvn.vault_hvn
-}
-
 output "vault_id" {
   description = "HCP Vault Cluster ID"
-  value = hcp_vault_cluster.vault_cluster.id
-}
-
-output "vault_namespace" {
-  description = "HCP Vault Cluster Namespace"
-  value = hcp_vault_cluster.vault_cluster.namespace
+  value = hcp_vault_cluster.cluster_setup.id
 }
 
 output "vault_public_endpoint_url" {
   description = "HCP Vault Cluster vault_public_endpoint_url"
-  value = hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
+  value = hcp_vault_cluster.cluster_setup.vault_public_endpoint_url
 }
 
-output "vault_version" {
-  description = "HCP Vault Cluster vault_version"
-  value = hcp_vault_cluster.vault_cluster.vault_version
-}
-
-output "vault_private_endpoint_url" {
-  description = "HCP Vault Cluster vault_public_endpoint_url"
-  value = hcp_vault_cluster.vault_cluster.vault_private_endpoint_url
+output "vault_admin_token" {
+  value = hcp_vault_cluster_admin_token.admin_token.token
+  sensitive = true
 }
