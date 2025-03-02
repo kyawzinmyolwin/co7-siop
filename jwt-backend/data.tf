c501dd183932e32c3d9bba3504306d2a9598af16
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2025-03-02 15:40:10 +0000

    testing auto

---
+++ jwt-backend/data.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "vault" {
+
+  backend = "remote"
+
+  config = {
+    organization = "kz-co7-siop1"
+    workspaces = {
+      name = "step1-cluster-setup"
+    }
+  }
+}
---
+++ jwt-backend/variables.tf
@@ -0,0 +1,13 @@
+variable "tfe_token" {
+  default = "Pf6w67wqf6w4Pw.atlasv1.e1HXT6uC7VnBaG2fIIOck5d2xxdDgyM3CZuZlCz5xJkfa2aqIAnUYYzBDhQZfHC7U8E"
+}
+variable "org_name" {
+  default = "kz-co7-siop1"
+}
+variable "repo_name" {
+  default = "co7-siop"
+}
+variable "oauth_token_id" { # Get from Terraform Cloud VCS provider settings
+  default = "ot-g6SJWQeetz8cUzfd"
+}
+
