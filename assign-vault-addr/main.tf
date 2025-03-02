7107fabc4b79be31f35db4263ccb071aaab03dfd
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2025-03-02 15:18:45 +0000

    add new working dir

---
+++ assign-vault-addr/main.tf
@@ -0,0 +1,18 @@
+#############################################
+#Step2 - Variables for JWT Backend
+#############################################
+resource "tfe_variable" "vault_addr" {
+  key          = "VAULT_ADDR"
+  value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
+  category     = "env"
+  workspace_id = tfe_workspace.jwt_backend.id
+  sensitive    = false
+}
+
+resource "tfe_variable" "vault_token" {
+  key          = "VAULT_TOKEN"
+  value        = data.terraform_remote_state.vault.outputs.vault_admin_token
+  category     = "env"
+  workspace_id = tfe_workspace.jwt_backend.id
+  sensitive    = true
+}
