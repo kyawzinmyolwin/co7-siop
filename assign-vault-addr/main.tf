dfd217aa18a4cb6dfa8afc3cd591ec57a99f962e
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2025-03-02 16:27:42 +0000

    redesign

--- assign-vault-addr/main.tf
+++ assign-vault-addr/main.tf
@@ -29,7 +29,7 @@ resource "tfe_variable" "vault_addr" {
   key          = "VAULT_ADDR"
   value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
   category     = "env"
-  workspace_id = data.tfe_workspace.jwt_backend.id
+  workspace_id = tfe_workspace.jwt_backend.id
   sensitive    = false
 }
 
@@ -37,6 +37,6 @@ resource "tfe_variable" "vault_token" {
   key          = "VAULT_TOKEN"
   value        = data.terraform_remote_state.vault.outputs.vault_admin_token
   category     = "env"
-  workspace_id = data.tfe_workspace.jwt_backend.id
+  workspace_id = tfe_workspace.jwt_backend.id
   sensitive    = true
 }
