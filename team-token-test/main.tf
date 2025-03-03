bef2189808dbcaeb01194021ae8f71cd279b40e4
kyazinmyolwin <kyawzin.workspace@gmail.com>
2025-03-03 05:48:24 +0000

    team token test

---
+++ team-token-test/main.tf
@@ -0,0 +1,35 @@
+provider "tfe" {
+token = tfe_team_token.test.token
+}
+
+#Generate Team Token
+
+resource "tfe_team" "test" {
+ name = "test"
+ organization = var.org_name
+}
+
+resource "tfe_team_token" "test" {
+ team_id = tfe_team.test.id
+}
+
+#############################################
+#Variables update for JWT Backend
+#############################################
+resource "tfe_variable" "vault_addr" {
+  key   = "VAULT_ADDR"
+  # value = var.vault_addr
+  value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
+  category     = "env"
+  workspace_id = data.tfe_workspace.workspace_name.id
+  sensitive    = true
+}
+
+resource "tfe_variable" "vault_token" {
+  key   = "VAULT_TOKEN"
+  # value = var.vault_token
+  value        = data.terraform_remote_state.vault.outputs.vault_admin_token
+  category     = "env"
+  workspace_id = data.tfe_workspace.workspace_name.id
+  sensitive    = true
+}
---
+++ team-token-test/variables.tf
@@ -0,0 +1,21 @@
+variable "tfc_token" {
+  default = ""
+}
+variable "org_name" {
+  default = "hc-siop2-org"
+}
+variable "project_id" {
+  default = "hc-siop2-proj"
+}
+variable "workspace_name" {
+  default = "tfe_workspace_test"
+}
+variable "repo_name" {
+  default = "co7-siop"
+}
+variable "vault_addr" {
+  default = "vault_addr_test_1.4"
+}
+variable "vault_token" {
+  default = "vault_token_test_1.4"
+}
