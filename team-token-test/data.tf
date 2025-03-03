e8f7a6220a19d89b743f8eca1d043030d6e8402e
kyazinmyolwin <kyawzin.workspace@gmail.com>
2025-03-03 07:55:25 +0000

    clean up folders

---
+++ team-token-test/data.tf
@@ -0,0 +1,24 @@
+#############################################
+#Getting Vault Address & Token - Only uncomment after cluster up
+#############################################
+data "terraform_remote_state" "vault" {
+
+  backend = "remote"
+
+  config = {
+    organization = "hc-siop2-org"
+    workspaces = {
+      name = "step1-vault-cluster"
+    }
+  }
+}
+# data "tfe_workspace" "step2_jwt_backend" {
+#   name         = var.workspace_name
+#   organization = var.org_name
+# }
+
+
+data "tfe_workspace" "workspace_name" {
+  name         = var.workspace_name
+  organization = var.org_name
+}
--- team-token-test/main.tf
+++ team-token-test/main.tf
@@ -1,25 +1,30 @@
-provider "tfe" {
-token = tfe_team_token.test.token
-}
+
 
 #Generate Team Token
 
 resource "tfe_team" "test" {
- name = "test"
- organization = var.org_name
+  name         = "test"
+  organization = var.org_name
 }
 
 resource "tfe_team_token" "test" {
- team_id = tfe_team.test.id
+  team_id = tfe_team.test.id
 }
 
+output "token_value" {
+  value     = tfe_team_token.test.token
+  sensitive = true
+}
+provider "tfe" {
+  token = data.tfe_team.test.
+}
 #############################################
 #Variables update for JWT Backend
 #############################################
 resource "tfe_variable" "vault_addr" {
   key   = "VAULT_ADDR"
-  # value = var.vault_addr
-  value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
+  value = var.vault_addr
+  # value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
   category     = "env"
   workspace_id = data.tfe_workspace.workspace_name.id
   sensitive    = true
@@ -27,8 +32,8 @@ resource "tfe_variable" "vault_addr" {
 
 resource "tfe_variable" "vault_token" {
   key   = "VAULT_TOKEN"
-  # value = var.vault_token
-  value        = data.terraform_remote_state.vault.outputs.vault_admin_token
+  value = var.vault_token
+  # value        = data.terraform_remote_state.vault.outputs.vault_admin_token
   category     = "env"
   workspace_id = data.tfe_workspace.workspace_name.id
   sensitive    = true
