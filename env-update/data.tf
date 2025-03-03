10a6502d0bd7b261c3334afa8b18c24631398e79
kyazinmyolwin <kyawzin.workspace@gmail.com>
2025-03-03 04:45:00 +0000

    test remote state

--- env-update/data.tf
+++ env-update/data.tf
@@ -1,17 +1,17 @@
 #############################################
 #Getting Vault Address & Token - Only uncomment after cluster up
 #############################################
-# data "terraform_remote_state" "vault" {
+data "terraform_remote_state" "vault" {
 
-#   backend = "remote"
+  backend = "remote"
 
-#   config = {
-#     organization = "kz-co7-siop1"
-#     workspaces = {
-#       name = "step1-cluster-setup"
-#     }
-#   }
-# }
+  config = {
+    organization = "kz-co7-siop1"
+    workspaces = {
+      name = "step1-cluster-setup"
+    }
+  }
+}
 # data "tfe_workspace" "step2_jwt_backend" {
 #   name         = var.workspace_name
 #   organization = var.org_name
--- env-update/main.tf
+++ env-update/main.tf
@@ -7,8 +7,8 @@ provider "tfe" {
 #############################################
 resource "tfe_variable" "vault_addr" {
   key   = "VAULT_ADDR"
-  value = var.vault_addr
-  # value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
+  # value = var.vault_addr
+  value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
   category     = "env"
   workspace_id = data.tfe_workspace.workspace_name.id
   sensitive    = false
@@ -16,8 +16,8 @@ resource "tfe_variable" "vault_addr" {
 
 resource "tfe_variable" "vault_token" {
   key   = "VAULT_TOKEN"
-  value = var.vault_token
-  # value        = data.terraform_remote_state.vault.outputs.vault_admin_token
+  # value = var.vault_token
+  value        = data.terraform_remote_state.vault.outputs.vault_admin_token
   category     = "env"
   workspace_id = data.tfe_workspace.workspace_name.id
   sensitive    = false
