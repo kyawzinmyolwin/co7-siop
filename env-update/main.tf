f7df02bd2f70b84a70fcb21630b363ab385f6739
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2025-03-03 10:19:28 +0000

    error checking1.1

--- env-update/main.tf
+++ env-update/main.tf
@@ -25,10 +25,10 @@ resource "tfe_variable" "vault_token" {
 ###############################################
 #Variables update for Step3 AWS Secret Backend
 ###############################################
-resource "tfe_variable" "tfc_vault_addr" {
-  key          = "TFC_VAULT_ADDR"
-  value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
-  category     = "env"
-  workspace_id = data.tfe_workspace.step3_aws_secret_backend.id
-  sensitive    = true
-}
+# resource "tfe_variable" "tfc_vault_addr" {
+#   key          = "TFC_VAULT_ADDR"
+#   value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
+#   category     = "env"
+#   workspace_id = data.tfe_workspace.step3_aws_secret_backend.id
+#   sensitive    = true
+# }
