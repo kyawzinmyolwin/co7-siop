e54c45ea66ff406e8e123ca14afbbd860e9ea934
kyazinmyolwin <kyawzin.workspace@gmail.com>
2025-03-03 02:55:14 +0000

    test-env 1.1

--- env-update/main.tf
+++ env-update/main.tf
@@ -3,7 +3,7 @@
 #############################################
 resource "tfe_variable" "vault_addr" {
   key   = "VAULT_ADDR"
-  value = "vault_addr_test"
+  value = "vault_addr_test_1.1"
   # value        = data.terraform_remote_state.vault.outputs.vault_public_endpoint_url
   category     = "env"
   workspace_id = data.tfe_workspace.step2_jwt_backend.id
@@ -12,7 +12,7 @@ resource "tfe_variable" "vault_addr" {
 
 resource "tfe_variable" "vault_token" {
   key   = "VAULT_TOKEN"
-  value = "vault_token_test"
+  value = "vault_token_test_1.1"
   # value        = data.terraform_remote_state.vault.outputs.vault_admin_token
   category     = "env"
   workspace_id = data.tfe_workspace.step2_jwt_backend.id
